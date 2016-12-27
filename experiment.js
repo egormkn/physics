0974e015713721d66d1cba4db9725e97655c6d48
Egor Makarenko <egormkn@yandex.ru>
2016-12-27 16:06:29 +0300

    Ray inside lens

--- experiment.js
+++ experiment.js
@@ -14,7 +14,7 @@ var Experiment = {
         n1: 1,
         n2: 1.33,
         lensRadius: 50,
-        delta: 1.0
+        delta: -0.5
     },
     getContext: function (sizeChanged) {
         if (this.ctx === undefined) {
@@ -115,14 +115,14 @@ var Experiment = {
             h = this.canvas.height;
         ctx.clearRect(-w / 2, -h / 2, w, h);
 
-        var coordY = Experiment.physics.lensRadius * Experiment.physics.delta;
+        var coordY = Experiment.physics.lensRadius * -Experiment.physics.delta;
 
         // Draw dashed lines
         ctx.setLineDash([5, 3]); /*dashes are 5px and spaces are 3px*/
         ctx.strokeStyle = "rgba(0, 0, 0, 0.4)";
         ctx.beginPath();
-        ctx.moveTo(-w / 2, coordY);
-        ctx.lineTo(w / 2, coordY);
+        ctx.moveTo(-w / 2, -coordY);
+        ctx.lineTo(w / 2, -coordY);
         ctx.moveTo(-w / 2, 0);
         ctx.lineTo(w / 2, 0);
         ctx.moveTo(0, -h / 2);
@@ -141,40 +141,95 @@ var Experiment = {
         ctx.fill(lens);
         ctx.stroke(lens);
 
-
         ctx.lineWidth = 1;
 
         // Draw main ray
         ctx.strokeStyle = "rgba(100, 0, 0, 1.0)";
         ctx.beginPath();
         ctx.moveTo(Experiment.physics.ray.x, Experiment.physics.ray.y);
-        ctx.lineTo(0, coordY);
+        ctx.lineTo(0, -coordY);
         ctx.stroke();
 
         // Draw reflected ray
         ctx.strokeStyle = "rgba(0, 0, 100, 1.0)";
         ctx.beginPath();
-        ctx.moveTo(Experiment.physics.ray.x, 2*coordY - Experiment.physics.ray.y);
-        ctx.lineTo(0, coordY);
+        ctx.moveTo(Experiment.physics.ray.x, 2 * -coordY - Experiment.physics.ray.y);
+        ctx.lineTo(0, -coordY);
         ctx.stroke();
 
         ctx.strokeStyle = "rgba(0, 0, 0, 1.0)";
 
-        var rayLength = Math.sqrt(Math.pow(Experiment.physics.ray.x, 2) + Math.pow(coordY - Experiment.physics.ray.y, 2));
+        var rayLength = Math.sqrt(Math.pow(Experiment.physics.ray.x, 2) + Math.pow(-coordY - Experiment.physics.ray.y, 2));
+        var sinFi2 = (Experiment.physics.n1 / Experiment.physics.n2) * (Math.abs(-coordY - Experiment.physics.ray.y) / rayLength);
+        var tgFi2 = Math.tan(Math.asin(sinFi2)) * (-coordY - Experiment.physics.ray.y > 0 ? -1 : 1);
 
-        var sinFi2 = (Experiment.physics.n1 / Experiment.physics.n2) * (Math.abs(coordY - Experiment.physics.ray.y) / rayLength);
-        var newY = rayLength * sinFi2 * (coordY - Experiment.physics.ray.y > 0 ? 1 : -1) + coordY;
-        var newX = Math.sqrt(Math.pow(rayLength, 2) - Math.pow(newY - coordY, 2));
-        
+        var a = tgFi2 * tgFi2 + 1;
+        var b = 2 * tgFi2 * coordY;
+        var c = coordY * coordY - Math.pow(Experiment.physics.lensRadius, 2);
+
+        var solution = solveSqrEquation(a, b, c);
+        var newX = 0, newY = 0;
+        if (solution.length > 0) {
+            solution = Math.max(solution[0], solution[1]);
+            newX = solution;
+            newY = coordY + tgFi2 * newX;
+        }
+
+        // Draw ray inside lens
         ctx.beginPath();
-        ctx.moveTo(newX, newY);
-        ctx.lineTo(0, coordY);
+        ctx.moveTo(newX, -newY);
+        ctx.lineTo(0, -coordY);
         ctx.stroke();
 
 
-        ctx.fillRect(0, Experiment.physics.lensRadius * Experiment.physics.delta, 10, 10);
+        // Draw normal line inside lens
+        ctx.setLineDash([5, 3]); /*dashes are 5px and spaces are 3px*/
+        ctx.strokeStyle = "rgba(0, 0, 0, 0.4)";
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(newX*2, -newY*2);
+        ctx.stroke();
+        ctx.setLineDash([0, 0]);
+        ctx.strokeStyle = "rgba(0, 0, 0, 1.0)";
+
+
+        // TODO
+        
+        // var angleRefraction1 = Math.asin(sinFi2) * 180 / Math.PI;
+        var anglePsi2 = Math.atan(Math.abs(newY) / newX) - Math.asin(sinFi2);
+        console.log(Math.min(anglePsi2, Math.PI - anglePsi2) * 180 / Math.PI);
+        var derivative = (newY - coordY) / newX;
+
+        var sinPsi2 = 0;
+        var sinPsi1 = (Experiment.physics.n2 / Experiment.physics.n1) * Math.sin(sinPsi2);
+
+        var tanRay3 = Math.tan(Math.atan((newY) / (newX)) - Math.asin(sinPsi1));
+
+        /*ctx.beginPath();
+        ctx.moveTo(newX, newY -coordY);
+        ctx.lineTo(newX + 100, tanRay3*(100) + newY -coordY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(100, 100*derivative);
+        ctx.lineTo(0, 0);
+        ctx.stroke();*/
+
+        ctx.fillStyle = "rgba(255, 0, 0, 0.6)";
+        ctx.fillRect(-2, Experiment.physics.lensRadius * Experiment.physics.delta - 2, 4, 4);
     }
 };
+
+function solveSqrEquation(a, b, c) { // ax^2 + bx + c = 0
+    var D = b * b - 4 * a * c;
+    if (D < 0) {
+        return [];
+    }
+    var sqrtD = Math.sqrt(D);
+    var x1 = (-b - sqrtD) / (2 * a);
+    var x2 = (-b + sqrtD) / (2 * a);
+    return [x1, x2];
+}
 
 /*window.onresize = function () {
     Experiment.setWidth("auto");
